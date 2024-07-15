2e67e4f86e89ba460bfe810c54e5ef10bacda470
Thien Nguyen <thien.nm2905@gmail.com>
2024-07-15 21:09:49 +0700

    original

--- index.js
+++ index.js
@@ -4,14 +4,15 @@ const axios = require('axios');
 const list = [
   'https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts',
   'https://raw.githubusercontent.com/bigdargon/hostsVN/master/filters/adservers-all.txt',
-  // 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/dnsmasq/pro.txt',
+  'https://abpvn.com/android/abpvn.txt',
+  'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/dnsmasq/pro.txt',
 ];
 async function run() {
   const blocklist = [];
   for (const url of list) {
     const content = await axios.get(url);
     content.data.split('\n').map((line) => {
-      const match = new RegExp(/^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s*|(@@)?\|?\||local=\/)(([\w*-]+\.)*[\w*-]+\.[\w*-]+)/, 'mui').exec(line);
+      const match = new RegExp(/^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s*|0\s*|(@@)?\|?\||local=\/)(([\w*-]+\.)*[\w*-]+\.[\w*-]+)/, 'mui').exec(line);
       if (match) {
         blocklist.push('local=/' + match[3].replace(new RegExp(/^www\./, 'mui'), '') + '/');
       }
@@ -20,6 +21,5 @@ async function run() {
   const data = [...new Set(blocklist)].sort();
   console.log(data.length);
   fs.writeFileSync(path.join('blocklist'), data.join('\n'));
-  // fs.writeFileSync(path.join('dnsmasq.hosts'), data.join('\n'));
 }
 run();
