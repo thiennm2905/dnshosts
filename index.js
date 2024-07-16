a753c854a0e697e8625fba461005a5e4a44d55b7
Thien Nguyen <thien.nm2905@gmail.com>
2024-07-16 18:01:14 +0700

    original

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
 const list = [
-  // 'https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts',
+  'https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts',
   // 'https://raw.githubusercontent.com/bigdargon/hostsVN/master/filters/adservers-all.txt',
   // 'https://abpvn.com/android/abpvn.txt',
   'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/dnsmasq/ultimate.txt',
 ];
+
 async function run() {
   const blocklist = [];
   for (const url of list) {
@@ -14,7 +15,6 @@ async function run() {
     content.data.split('\n').map((line) => {
       const match = new RegExp(/^([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\s*|0\s*|\|\||local=\/)?(([\w*-]+\.)*[\w*-]+\.[\w*-]+)/, 'mui').exec(line);
       if (match) {
-        // blocklist.push('0.0.0.0 ' + match[2].replace(new RegExp(/^www\./, 'mui'), ''));
         blocklist.push('local=/' + match[2].replace(new RegExp(/^www\./, 'mui'), '') + '/');
       }
     });
@@ -23,4 +23,5 @@ async function run() {
   console.log(data.length);
   fs.writeFileSync(path.join('blocklist_ultimate'), data.join('\n'));
 }
+
 run();
