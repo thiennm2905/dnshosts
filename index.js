6b2344f8f5b57772e64dd78c6331c2d8e8fd6c8d
Thien Nguyen <thien.nm2905@gmail.com>
2024-07-18 22:29:00 +0700

    original

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const list = [
   'https://raw.githubusercontent.com/bigdargon/hostsVN/master/hosts',
   'https://raw.githubusercontent.com/bigdargon/hostsVN/master/filters/adservers-all.txt',
   'https://abpvn.com/android/abpvn.txt',
-  'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/dnsmasq/multi.txt',
+  // 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/dnsmasq/multi.txt',
   // 'https://raw.githubusercontent.com/hagezi/dns-blocklists/main/dnsmasq/ultimate.txt',
 ];
 
@@ -22,7 +22,7 @@ async function run() {
   }
   const data = [...new Set(blocklist)].sort();
   console.log(data.length);
-  fs.writeFileSync(path.join('blocklist'), data.join('\n'));
+  fs.writeFileSync(path.join('blocklist_vn'), data.join('\n'));
 }
 
 run();
